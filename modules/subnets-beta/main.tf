b88d159f21918c519fe67a8ca8b8bcc5b2a5ac54
sabornid <106259678+sabornid@users.noreply.github.com>
2022-05-26 10:19:23 +0530

    Update main.tf

--- modules/subnets-beta/main.tf
+++ modules/subnets-beta/main.tf
@@ -4,3 +4,15 @@ resource "google_compute_subnetwork" "new_subnetwork" {
   network       = "${var.vpc}"
   ip_cidr_range = "${var.ip_cidr_range}"
 }
+
+
+module "subnet1"{
+  source = "github.com/asdasd"
+  vpc    = module.net-vpc1.output
+  ip_cidr_range = "10.0.0.0/24"
+  name          = "prod"
+  region        = "europe-west1"
+  secondary_ip_range = {
+    pods     = "172.16.0.0/20"
+    services = "192.168.0.0/24"
+  }
